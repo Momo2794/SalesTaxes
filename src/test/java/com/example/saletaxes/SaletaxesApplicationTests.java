1b1589ce3bc1fc66cd391f7d38a375b151db020c
Momo2794 <mohamed_haddad@hotmail.de>
2021-10-14 23:34:59 +0200

    RAW Beginning

---
+++ src/test/java/com/example/saletaxes/SaletaxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saletaxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaletaxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
