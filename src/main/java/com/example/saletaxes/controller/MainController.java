1552382d622a06193e7f2a2703f01e0118aa8c7e
Momo2794 <mohamed_haddad@hotmail.de>
2021-10-15 15:14:31 +0200

    Development of the Backend: receiving the data of products from a json file with the help of a controller, entity, repository/dao, service

--- src/main/java/com/example/saletaxes/controller/MainController.java
+++ src/main/java/com/example/saletaxes/controller/MainController.java
@@ -1,7 +1,44 @@
 package com.example.saletaxes.controller;
 
+import com.example.saletaxes.entity.Product;
+import com.example.saletaxes.service.ProductService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(value = "/data/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    private List<Product> getAllData() {
+        List<Product> productList = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<Product>> typeReference = new TypeReference<List<Product>>() {};
+        InputStream inputStream = TypeReference.class.getResourceAsStream("/json/Product.json");
+
+        try {
+            objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            List<Product> products = objectMapper.readValue(inputStream, typeReference);
+            productList = products;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return productList;
+    }
 }
--- src/main/java/com/example/saletaxes/repository/ProductRepo.java
+++ src/main/java/com/example/saletaxes/repository/ProductRepo.java
@@ -1,7 +1,9 @@
 package com.example.saletaxes.repository;
 
+import com.example.saletaxes.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepo {
+public interface ProductRepo extends JpaRepository<Product, Long> {
 }
